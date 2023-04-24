67a1e639aeaa763475130f751f414d693957fc15
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-24 10:27:20 +0000

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
