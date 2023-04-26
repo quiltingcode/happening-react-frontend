8c9208c04b9b7dd8fc54003f513b29cc218123a4
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-26 14:36:11 +0000

    feat: Style navbar items #1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,39 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../assets/logo-transparent.png';
-import brandname from '../assets/brand-transparent-cropped.png';
+import styles from '../styles/NavBar.module.css';
 
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
             <img src={logo} alt="logo" height="55"/>
-            <img src={brandname} alt="brandname" height="45" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <Nav.Link><i class="fas fa-home"></i> Home</Nav.Link>
-            <Nav.Link><i class="fas fa-sign-in-alt"></i> Sign in</Nav.Link>
-            <Nav.Link><i class="fas fa-user-plus"></i> Sign up</Nav.Link>
+            <Nav.Link><i className="fas fa-home"></i> Home</Nav.Link>
+            <Nav.Link><i className="fas fa-home"></i> Feed</Nav.Link>
+            <Nav.Link><i className="fas fa-home"></i> My Events</Nav.Link>
+            <Nav.Link><i className="fas fa-home"></i>Reviews</Nav.Link>
+            <NavDropdown 
+              title={
+                <span>
+                    <i class="fas fa-user-alt"></i>
+                </span>
+              }
+              id="basic-nav-dropdown" 
+            >
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+              <NavDropdown.Item><i className="fas fa-sign-in-alt"></i>Sign in</NavDropdown.Item>
+              <NavDropdown.Item><i className="fas fa-user-plus"></i>Sign up</NavDropdown.Item>
+              <NavDropdown.Item>Logout</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
