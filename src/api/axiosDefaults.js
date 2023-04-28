bf30f1e6558b79b4488d8cfe0ffffd0c346d7c9b
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-04-28 08:46:40 +0000

    feat: setup axios interceptors to extend refresh token

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://happening-api-kelz.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
