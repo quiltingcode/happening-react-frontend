856aafcd57261d47dd0809c1f9ce1719e363bad3
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-05-01 11:15:03 +0000

    feat:setup icons for interested and click conditions #15

--- src/pages/events/Event.js
+++ src/pages/events/Event.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../../styles/Event.module.css'
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { Card, Media, OverlayTrigger } from 'react-bootstrap';
+import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom';
 import Avatar from '../../components/Avatar';
 
@@ -48,17 +48,35 @@ const Event = (props) => {
             <Card.Img src={image} alt={title} />
         </Link>
         <Card.Body>
-            {title && event_date <Card.Title className='text-center'>{title} - {event_date}</Card.Title>}
+            {title && event_date && <Card.Title className='text-center'>{title} - {event_date}</Card.Title> } 
             {description && <Card.Text>{description}</Card.Text>}
             <div>
                 {is_owner ? (
-                    <OverlayTrigger>
-                        <i class="fa-regular fa-face-grin-hearts"></i>
+                    <OverlayTrigger placement='top' overlay={<Tooltip>You can't be interested in your own event, sorry!</Tooltip>}>
+                        <i className="fa-thin fa-face-grin-wide"></i>
                     </OverlayTrigger>
-                ) : }
+                ) : interested_id ? (
+                    <span onClick={() => {}}>
+                        <i className={`fa-duotone fa-face-grin-wide ${styles.Interested}`} ></i>
+                    </span>
+                ) : currentUser ? (
+                    <span onClick={() => {}}>
+                        <i className={`fa-duotone fa-face-grin-wide ${styles.NotInterested}`} ></i>
+                    </span>
+                ) : (
+                    <OverlayTrigger placement='top' overlay={<Tooltip>Log in to show your interest!</Tooltip>}>
+                        <i className="fa-thin fa-face-grin-wide"></i>
+                    </OverlayTrigger>
+                    
+                )}
+                {interested_count}
+                
+                <Link to={`/events/${id}`}>
+                    <i className='far fa-comments'></i>
+                </Link>
+                {comments_count}
             </div>
         </Card.Body>
-        
     </Card>
   )
 }
