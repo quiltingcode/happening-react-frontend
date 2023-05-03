2e484b4f91dc54848dec213000e724b1ca7e6acf
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-05-03 16:56:08 +0000

    feat: request full list of events from API #7

--- src/pages/events/EventsPage.js
+++ src/pages/events/EventsPage.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import NoResults from "../../assets/no-results.png"
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/EventsPage.module.css";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import Event from "./Event";
+import Asset from "../../components/Asset";
 
-function EventsPage() {
+function EventsPage({ message, filter="" }) {
+
+  const [events, setEvents] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const { data } = await axiosReq.get(`/events/?${filter}`);
+        setEvents(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchEvents();
+  }, [filter, pathname]);
   
   return (
     <Row className="h-100">
@@ -17,7 +41,25 @@ function EventsPage() {
         <div className="d-lg-none">
           <p>Top events this month - mobile</p>
         </div>
-        <p>List of events here</p>
+        {hasLoaded ? (
+          <>
+          {events.results.length ? (
+            events.results.map(event => (
+              <Event key={event.id} {...event} setEvents={setEvents} />
+            ))
+          ) : (
+            <Container className={appStyles.Content}>
+              <Asset src={NoResults} message={message} />
+            </Container>
+          )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )
+      
+      }
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Top events this month - desktop</p>
