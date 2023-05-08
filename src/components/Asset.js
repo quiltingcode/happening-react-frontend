482e5bf6b6ae75aeb1a6d92867d44a78f93f45b7
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-05-08 09:28:24 +0000

    feat: Style loading spinner

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -5,7 +5,7 @@ import styles from "../styles/Asset.module.css";
 const Asset = ({ spinner, src, message }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
-      {spinner && <Spinner animation="border" />}
+      {spinner && <Spinner animation="border" className={styles.Spinner} />}
       {src && <img src={src} alt={message} />}
       {message && <p className="mt-4">{message}</p>}
     </div>
