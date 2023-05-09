f38b8854c0edd8e60c3cf42cce02b16dd2d6a661
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-05-09 13:36:56 +0000

    feat: Create Profile Data Context

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,37 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import appStyles from "../../App.module.css";
 import Container from 'react-bootstrap/Container';
-import { axiosReq } from '../../api/axiosDefaults';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from '../../components/Asset';
 import Profile from './Profile';
+import { useProfileData } from '../../contexts/ProfileDataContext';
 
 const PopularProfiles = () => {
 
-    const [profileData, setProfileData] = useState({
-        pageProfile: { results: [] },
-        popularProfiles: { results: [] },
-    });
-
-    const {popularProfiles} = profileData;
-    const currentUser = useCurrentUser();
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const { data } = await axiosReq.get(
-                    "/profiles/?ordering=-followers_count"
-                );
-                setProfileData((prevState) => ({
-                    ...prevState,
-                    popularProfiles: data,
-                }));
-            } catch (err) {
-                console.log(err)
-            }
-        };
-        handleMount();
-    }, [currentUser]);
+    const { popularProfiles } = useProfileData;
 
   return (
     <Container className={`${appStyles.Content} mb-3`}>
